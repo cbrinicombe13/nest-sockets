85b40bc8698b8900d5020b4c55bd61b8a5a3fa0b
Charlie Brinicombe <cbrinicombe13@googlemail.com>
2021-08-03 23:20:54 +0100

    Added custom nextjs pages and bootstrap

---
+++ src/client/pages/_document.tsx
@@ -0,0 +1,40 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossOrigin="anonymous"
+          />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div className="row">
+      <div className="col-12">
+        <div className="card">
+          <div className="card-body text-center">Hello World!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
